d5da5a8af211038fe9ab4784b8fb80fa57076ee7
Hyeongmin Park <thalsal@naver.com>
2023-12-07 17:33:24 +0900

    chore : fcm admin sdk 초기화 설정

---
+++ src/main/java/com/example/chatserver/config/FCMConfig.java
@@ -0,0 +1,24 @@
+package com.example.chatserver.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FCMConfig {
+
+    @PostConstruct
+    private void init() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("resources/security/Server-Security/fcm/tht-fcm-firebase-adminsdk-waqsh-7f9e6071b2.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
