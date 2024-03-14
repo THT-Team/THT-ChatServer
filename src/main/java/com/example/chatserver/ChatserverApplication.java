3c995f18b6b32da208e302cb09a7ecc241cf80f3
thalals <thalsal@naver.com>
2024-03-14 19:57:15 +0900

    feat : set timezone asia/seoul

--- src/main/java/com/example/chatserver/ChatserverApplication.java
+++ src/main/java/com/example/chatserver/ChatserverApplication.java
@@ -1,15 +1,22 @@
 package com.example.chatserver;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class ChatserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatserverApplication.class, args);
+    }
 
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
